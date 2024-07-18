ae6408b4e62d9b13326009564188141c4de5775e
Posandu Mapa <posandumapa@gmail.com>
2024-07-18 10:53:31 +0530

    Vercel!!

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
