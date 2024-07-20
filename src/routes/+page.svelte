f4fc63884562c4ee27f3039fa92f625c5a7f2bde
Posandu Mapa <posandumapa@gmail.com>
2024-07-20 17:06:42 +0530

    Add video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -429,7 +429,18 @@
 			time and reduce errors in the process.
 		</p>
 
-		<img src={Exams} alt="OCR Scanning" class="mx-auto max-w-full rounded-xl shadow-xl" />
+		<iframe
+			width="560"
+			height="315"
+			src="https://www.youtube.com/embed/uflurlBNCuk?si=I5s1bGF76crmX9Gv"
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerpolicy="strict-origin-when-cross-origin"
+			allowfullscreen
+			class="mx-auto rounded-xl shadow-xl"
+			loading="lazy"
+		></iframe>
 	</div>
 </section>
 
